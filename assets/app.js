402aa6b62140af351cca3d045c2babe5b9213bb1
aurele <aurelien.plazzotta@tutanota.com>
2024-06-07 14:50:15 +0200

    Added some style for Liste des personnages

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,18 @@
-import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.scss';
+import "./styles/app.scss";
+import { Tooltip, Toast, Popover } from "./bootstrap";
+import "./bootstrap";
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+// $(document).load(
+//   $(function () {
+//     $('[data-bs-toggle="popover"]').popover();
+//   })
+// );
+$(document).ready(function(){
+  $('[data-toggle="popover"]').popover();
+});
--- src/Controller/JouerController.php
+++ src/Controller/JouerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PersonnageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class JouerController extends AbstractController
 {
     #[Route('/jouer', name: 'app_jouer')]
-    public function index(): Response
+    public function index(PersonnageRepository $personnageRepository): Response
     {
+        $personnages = $personnageRepository->findAll();
         return $this->render('jouer/index.html.twig', [
-            'controller_name' => 'JouerController',
+            'personnages' => $personnages,
         ]);
     }
 }
